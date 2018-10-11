aa28a1ad975ec0e21e00ccaaf3e17e4095223004
Jillian <jjritchey@uchicago.edu>
2018-10-11 14:25:11 -0500

    git ignore

--- src/edu/virginia/engine/display/AnimatedSprite.java
+++ src/edu/virginia/engine/display/AnimatedSprite.java
@@ -2,6 +2,7 @@ package edu.virginia.engine.display;
 import edu.virginia.engine.util.GameClock;
 
 import java.util.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 public class AnimatedSprite extends Sprite{
@@ -9,7 +10,7 @@ public class AnimatedSprite extends Sprite{
     private ArrayList animations = new ArrayList();
     private Boolean playing;
     private String fileName;
-    private ArrayList frames = new ArrayList();
+    private ArrayList frames = new ArrayList<BufferedImage>();
     private int currentFrame;
     private int startFrame;
     private int endFrame;
@@ -18,10 +19,10 @@ public class AnimatedSprite extends Sprite{
 
     private GameClock gameClock;
 
-    public Sprite(String ID, String fileName) {
+    public AnimatedSprite(String ID, String fileName) {
         super(ID, fileName);
         gameClock = new GameClock();
-        animationSpeed = 2;
+        animationSpeed = DEFAULT_ANIMATION_SPEED;
     }
 
     public void initGameClock(){
