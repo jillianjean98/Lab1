f7c92b72e5974ad860bf4c02b5840055d179bfe9
Jillian <jjritchey@uchicago.edu>
2018-10-18 15:22:12 -0500

    draw and update methods

--- src/edu/virginia/engine/display/DisplayObjectContainer.java
+++ src/edu/virginia/engine/display/DisplayObjectContainer.java
@@ -81,5 +81,59 @@ public class DisplayObjectContainer extends DisplayObject{
 		}
 	}
 
-	
+	/**
+	 * Draws this image. This should be overloaded if a display object should
+	 * draw to the screen differently. This method is automatically invoked on
+	 * every frame.
+	 * */
+	@Override
+	public void draw(Graphics g) {
+
+		super.draw(g);
+
+			/*
+			 * Get the graphics and apply this objects transformations
+			 * (rotation, etc.)
+			 */
+			Graphics2D g2d = (Graphics2D) g;
+			applyTransformations(g2d);
+
+			/* Actually draw the image, perform the pivot point translation here */
+			for(DisplayObject child : children) {
+				child.draw(g2d);
+			}
+			/*
+			 * undo the transformations so this doesn't affect other display
+			 * objects
+			 */
+			reverseTransformations(g2d);
+		}
+	}
+
+	protected void applyTransformations(Graphics2D g2d){
+			g2d.translate(this.getPosition().x, this.getPosition().y);
+			g2d.rotate(Math.toRadians(this.getRotation()), this.getPivotPoint().x, this.getPivotPoint().y);
+			g2d.scale(this.getScaleX(), this.getScaleY());
+			float curAlpha;
+			this.setOldAlpha(curAlpha = ((AlphaComposite)
+					g2d.getComposite()).getAlpha());
+			g2d.setComposite(AlphaComposite.getInstance(3, curAlpha *
+					this.getAlpha()));
+
+	}
+
+	/**
+	 * Reverses transformations for this display object to the given graphics
+	 * object
+	 * */
+
+	protected void reverseTransformations(Graphics2D g2d){
+		g2d.setComposite(AlphaComposite.getInstance(3,
+				this.getOldAlpha()));
+		g2d.scale(0.5, 0.5);
+		g2d.rotate(Math.toRadians(-this.getRotation()), this.getPivotPoint().x, this.getPivotPoint().y);
+		g2d.translate(0,0);
+
+	}
+
 }
