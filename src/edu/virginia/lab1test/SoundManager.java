f3d55d014050835886a052a79e20e240890c0df6
lindseyma <lma2@stuy.edu>
2018-11-01 15:15:18 -0500

    should be done with music just have to test with wav files

--- src/edu/virginia/lab1test/SoundManager.java
+++ src/edu/virginia/lab1test/SoundManager.java
@@ -6,7 +6,9 @@ import java.io.*;
 
 public class SoundManager {
 
+    Clip music;
     public Map<String, Clip> soundEffectMap = new HashMap<>();
+    //public Map<String, Clip> musicMap = new HashMap<>();
 
     public void LoadSoundEffect(String id, String filename){
         Clip clip;
@@ -33,6 +35,28 @@ public class SoundManager {
         Clip curr = soundEffectMap.get(id);
         curr.setFramePosition(0);
         curr.start();
+    }
+
+    public void LoadMusic(String id, String filename){
+        try{
+            URL loc = this.getClass().getClassLoader().getResource(filename);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(loc);
+            music = AudioSystem.getClip();
+            music.open(audioInputStream);
+        }
+        catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void PlayMusic(String id){
+        music.setFramePosition(0);
+        music.start();
+        music.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
 }
